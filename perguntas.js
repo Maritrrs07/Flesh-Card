5890b63bfe83ca5b8407408babab4feb9aac2a50
Maritrrs07 <maria.ipanema.torres@escola.pr.gov.br>
2025-07-01 10:05:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'O que foi a Guerra Fria?',
+    'Conflito ideológico e político entre EUA (capitalismo) e URSS (socialismo) pós-Segunda Guerra, sem confronto militar direto, mas com corrida armamentista e conflitos indiretos.'
+)
+
+criaCartao(
+    'Biologia',
+    ' Reino Fungi?',
+    'Organismos eucariontes, heterótrofos, com parede celular de quitina.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Tipos de Sujeito',
+    'Simples, Composto, Oculto/Elíptico, Indeterminado, Inexistente'
+)
